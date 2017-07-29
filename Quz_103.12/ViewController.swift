519a3c7d1295ccaed4474b0b71bab8ad48dcdb4d
Jorge Guerra <jorgeguerra@DAL-38R-2017s-iMac.local>
2017-07-28 21:11:00 -0500

    Fixed it  Please enter the commit message for your changes. Lines starting

---
+++ Quz_103.12/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Quz_103.12
+//
+//  Created by Jorge Guerra on 7/28/17.
+//  Copyright © 2017 Jorge Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
